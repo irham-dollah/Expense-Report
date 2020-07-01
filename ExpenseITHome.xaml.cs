dd94a6cc301ab1f67c5bd5597e2c56bcccd10dfe
irham-dollah <irham8016@gmail.com>
2020-07-01 11:38:05 +0800

    Add project files.

---
+++ ExpenseITHome.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ExpenseIT
+{
+    /// <summary>
+    /// Interaction logic for ExpenseITHome.xaml
+    /// </summary>
+    public partial class ExpenseITHome : Page
+    {
+        public ExpenseITHome()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click (object sender, RoutedEventArgs e)
+        {
+            ExpenseReportPage report = new ExpenseReportPage(this.peopleListBox.SelectedItem);
+            this.NavigationService.Navigate(report);
+        }
+    }
+}
